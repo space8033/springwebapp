50682940083796db818d5ef69d2d3c085ae9a12a
space8033 <space8237@naver.com>
2023-07-17 16:27:20 +0900

    test

--- src/main/java/com/mycompany/springwebapp/controller/HouseController.java
+++ src/main/java/com/mycompany/springwebapp/controller/HouseController.java
@@ -1,6 +1,19 @@
 package com.mycompany.springwebapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
 public class HouseController {
 	
+	@RequestMapping("/")
+	public String hello() {
+		log.info("실행1");
+		log.info("실행2");
+		return "index";
+	}
 	
 }
