7644f5b7e4dab2defceacc97308c3a5a3e593434
space8033 <space8237@naver.com>
2023-07-18 10:25:24 +0900

    log 삭제

--- src/main/java/com/mycompany/springwebapp/controller/HomeController.java
+++ src/main/java/com/mycompany/springwebapp/controller/HomeController.java
@@ -12,11 +12,6 @@ public class HomeController {
 	@RequestMapping("/")
 	public String index() {
 		log.info("실행1");
-		log.info("실행2");
-		log.info("실행3");
-		log.info("실행4");
-		log.info("실행7");
-		log.info("실행8");
 		
 		return "index";
 	}
